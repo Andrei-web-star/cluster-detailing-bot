825406beafb67c44229cd2df78207ddbc5ae5319
Andrei-web-star <pavlenko300@gmail.com>
2025-09-11 02:10:26 +0900

    Update test_env.py

--- test_env.py
+++ test_env.py
@@ -1,10 +1,5 @@
 import os
 
-print("=== ENV DEBUG START ===")
-print("BOT_TOKEN =", repr(os.getenv("BOT_TOKEN")))
-print("ADMIN_CHAT_ID =", repr(os.getenv("ADMIN_CHAT_ID")))
-print("LOGO_PATH =", repr(os.getenv("LOGO_PATH")))
-print("=== ENV DEBUG END ===")
-
-import time
-time.sleep(300)  # держим контейнер живым 5 минут
+print("BOT_TOKEN from env:", repr(os.getenv("BOT_TOKEN")))
+print("ADMIN_CHAT_ID from env:", repr(os.getenv("ADMIN_CHAT_ID")))
+print("LOGO_PATH from env:", repr(os.getenv("LOGO_PATH")))
