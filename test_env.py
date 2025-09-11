59486155ccc06e2fce4b7a3fe2be7edfed4859f4
Andrei-web-star <pavlenko300@gmail.com>
2025-09-11 20:30:11 +0900

    Delete test_env.py

--- test_env.py
+++
@@ -1,5 +0,0 @@
-import os
-
-print("BOT_TOKEN from env:", repr(os.getenv("BOT_TOKEN")))
-print("ADMIN_CHAT_ID from env:", repr(os.getenv("ADMIN_CHAT_ID")))
-print("LOGO_PATH from env:", repr(os.getenv("LOGO_PATH")))
