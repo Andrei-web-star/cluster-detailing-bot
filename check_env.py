02c1ffaffcfc9dfcaa59431524aae8aa244c1d51
Andrei-web-star <pavlenko300@gmail.com>
2025-09-11 21:00:37 +0900

    Update check_env.py

--- check_env.py
+++ check_env.py
@@ -3,3 +3,4 @@ import os
 print("BOT_TOKEN =", os.getenv("BOT_TOKEN"))
 print("ADMIN_CHAT_ID =", os.getenv("ADMIN_CHAT_ID"))
 print("LOGO_PATH =", os.getenv("LOGO_PATH"))
+print("TEST_VAR:", os.getenv("TEST_VAR"))
