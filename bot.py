8b59683aa08e00ae7c1492cb49d8f30d1adeb186
Andrei-web-star <pavlenko300@gmail.com>
2025-09-11 23:15:58 +0900

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,25 +1,14 @@
 import os
 
 print("=== DEBUG VARIABLES ===")
-for key, value in os.environ.items():
-    if "BOT" in key or "TOKEN" in key or "CHAT" in key or "LOGO" in key:
-        print(f"{key} = {value}")
+for k, v in os.environ.items():
+    if "BOT" in k or "TOKEN" in k or "CHAT" in k:
+        print(f"{k} = {v}")
 print("=== END DEBUG ===")
-
-import asyncio
-from aiogram import Bot, Dispatcher
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 if not BOT_TOKEN:
     raise RuntimeError("BOT_TOKEN is not set!")
 
-bot = Bot(token=BOT_TOKEN)
-dp = Dispatcher()
-
-async def main():
-    print("Bot started OK ✅")
-    await dp.start_polling(bot)
-
-if __name__ == "__main__":
-    asyncio.run(main())
+print("BOT_TOKEN is OK ✅")
